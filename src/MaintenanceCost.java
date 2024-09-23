fd00412c1b7dc0bc4c191a8a808e791d80fc3475
D'Angelo Capell <dangelocapelljr@gmail.com>
2024-09-23 11:21:26 -0400

    Initial commit

---
+++ src/MaintenanceCost.java
@@ -0,0 +1,10 @@
+public class MaintenanceCost{
+    public static void main(String[] args) {
+        double springCost = 126.0;
+        double summerCost = 294.0;
+        double fallCost = 300.0;
+        double winterCost = 478.0;
+        double yearlyCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("Total yearly maintenance cost: " + yearlyCost);
+    }
+}
